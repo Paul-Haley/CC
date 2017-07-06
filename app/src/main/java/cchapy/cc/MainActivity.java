c62daffd417846b6d09a9ff414d5b77fa30f5a9b
HFaulkner <hayley.faulkner7@gmail.com>
2017-07-06 14:03:42 +1000

    Added base holder profile activity

--- app/src/main/java/cchapy/cc/MainActivity.java
+++ app/src/main/java/cchapy/cc/MainActivity.java
@@ -1,13 +1,33 @@
 package cchapy.cc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get application context
+        mContext = getApplicationContext();
+
+        //Get self activity
+        mActivity = MainActivity.this;
+    }
+
+    public void viewProfile(View view) {
+        //Create view profile intent
+        Intent intent = new Intent(this, DisplayProfileActivity.class);
+        startActivity(intent);
     }
 }

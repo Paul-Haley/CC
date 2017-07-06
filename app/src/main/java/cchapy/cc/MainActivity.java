266d9e8b1a6c79cb8f465955fdf004be5fdc7d9e
Callum Bryson <callumjbryson@gmail.com>
2017-07-06 12:57:06 +1000

    Misc changes

--- app/src/main/java/cchapy/cc/MainActivity.java
+++ app/src/main/java/cchapy/cc/MainActivity.java
@@ -1,5 +1,7 @@
 package cchapy.cc;
 
+import android.content.res.Resources;
+import android.support.v4.media.RatingCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 

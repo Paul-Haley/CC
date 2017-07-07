e0fdce9129fad5c4f437d1bf0a351f51d9949ebd
Callum Bryson <callumjbryson@gmail.com>
2017-07-07 16:20:26 +1000

    Added Avatar Display Screen

---
+++ app/src/main/java/cchapy/cc/AvatarActivity.java
@@ -0,0 +1,13 @@
+package cchapy.cc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvatarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avatar);
+    }
+}

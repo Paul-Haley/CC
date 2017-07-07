21a50a2570321e4a74de6b59b6aeeaca28797106
Callum Bryson <callumjbryson@gmail.com>
2017-07-07 18:00:18 +1000

    Removed dialog titlebar

--- app/src/main/java/cchapy/cc/DisplayPopUpProfileActivity.java
+++ app/src/main/java/cchapy/cc/DisplayPopUpProfileActivity.java
@@ -3,12 +3,14 @@ package cchapy.cc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class DisplayPopUpProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_display_pop_up_profile);
 
         //get the intent that started this activity

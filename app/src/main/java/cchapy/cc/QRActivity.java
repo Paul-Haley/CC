89c4f1cc3b6df2c5c15488266035b9722af4ccbf
Paul Haley <paul.haley@bigpond.com>
2017-07-07 09:47:14 +0800

    QR reader now in vertical orientation

--- app/src/main/java/cchapy/cc/QRActivity.java
+++ app/src/main/java/cchapy/cc/QRActivity.java
@@ -17,6 +17,8 @@ public class QRActivity extends AppCompatActivity {
 
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan a CC QR code for Leaves and Avatars");
+        integrator.setOrientationLocked(false);
         integrator.setBeepEnabled(true);
         integrator.initiateScan(); // cause qr code scanner to start
 

0ad3b4c32ec0d10cacb7af5a6f5885ebcc591847
Callum Bryson <callumjbryson@gmail.com>
2017-07-10 16:29:41 +1000

    Misc layout changes

--- app/src/main/java/cchapy/cc/DiscountActivity.java
+++ app/src/main/java/cchapy/cc/DiscountActivity.java
@@ -3,11 +3,25 @@ package cchapy.cc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DiscountActivity extends AppCompatActivity {
+import cchapy.cc.dummy.DummyContent;
+
+public class DiscountActivity extends AppCompatActivity
+        implements VoucherListingFragment.OnListFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discount);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.discount_inventory_contents, new VoucherListingFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }

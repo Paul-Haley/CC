3d5cba6ac708720df85c6f2740dd793c2b2427fc
HFaulkner <hayley.faulkner7@gmail.com>
2017-07-10 16:22:39 +1000

    Added Database Contact, Database Helper and crap code in leaderboard to test them

--- app/src/main/java/cchapy/cc/leaderboardActivity.java
+++ app/src/main/java/cchapy/cc/leaderboardActivity.java
@@ -1,20 +1,30 @@
 package cchapy.cc;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cchapy.cc.dummy.UserContent;
 
 public class leaderboardActivity extends AppCompatActivity
     implements UserFragment.OnListFragmentInteractionListener {
 
+    DatabaseHelper mDbHelper = new DatabaseHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
@@ -57,8 +67,72 @@ public class leaderboardActivity extends AppCompatActivity
         startActivity(intent);
     }
 
+
+    public void addDummyUser(View view){
+
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.UsersTable.COLUMN_NAME_USERNAME, "from the database");
+        values.put(DatabaseContract.UsersTable.COLUMN_NAME_CITY, "Cityyy");
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(DatabaseContract.UsersTable.TABLE_NAME, null, values);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Added to db!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        readDatabase();
+    }
+
+    /*public void addDummyUser(View view){
+        // Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.UsersTable.COLUMN_NAME_USERNAME, "from the database");
+        values.put(DatabaseContract.UsersTable.COLUMN_NAME_CITY, "Cityyy");
+
+        // Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(DatabaseContract.UsersTable.TABLE_NAME, null, values);
+
+        readDatabase();
+    }*/
+
     @Override
     public void onListFragmentInteraction(UserContent.User user) {
         viewPopUpProfile(user);
     }
+
+
+    public void readDatabase() {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String q = "SELECT * FROM users";
+        Cursor mCursor = db.rawQuery(q, null);
+
+        mCursor.moveToFirst();
+        int i = 0;
+        do {
+            String username = mCursor.getString(mCursor.getColumnIndex(DatabaseContract.UsersTable.COLUMN_NAME_USERNAME));
+            Context context = getApplicationContext();
+
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, Integer.toString(i) + username, duration);
+            toast.show();
+
+            i++;
+        } while (mCursor.moveToNext());
+        mCursor.close();
+
+    }
 }
+

ff06caadb6dff7c13988af0ab5ddebc277b78d18
Paul Haley <paul.haley@bigpond.com>
2017-07-12 09:46:45 +0800

    changes needed for avatars in store

--- app/src/main/java/cchapy/cc/ShopActivity.java
+++ app/src/main/java/cchapy/cc/ShopActivity.java
@@ -52,7 +52,7 @@ public class ShopActivity extends AppCompatActivity
     }
 
     @Override
-    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+    public void onListFragmentInteraction(Avatar item) {
 
     }
 }
